f2e8436ce9e71513b5d0fc5b19328240a007a3eb
mahdi <10framerategmail.com>
2020-08-12 11:28:11 +0100

    first commit and cp

---
+++ New Unity Project (9)/Assets/ballmovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ballmovement : MonoBehaviour
+{
+    public Rigidbody2D rb2d;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+
+    }
+
+
+}
---
+++ New Unity Project (9)/Assets/movement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    public float speed = 30;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+   
+   
+
+    void FixedUpdate()
+    {
+        //This is our GetAxisRaw input 
+        float v = Input.GetAxisRaw("Vertical") * speed;
+        //Just calling the Rigidbody2D component to change its Velocity value
+        GetComponent<Rigidbody2D>().velocity = new Vector2(0, v);
+    }
+}
