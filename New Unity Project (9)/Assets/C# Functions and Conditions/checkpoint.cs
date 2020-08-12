2de2ae450ca5b7d7fd7321194b0ee7d077197e78
mahdi <10framerategmail.com>
2020-08-12 12:45:16 +0100

    PongSuperSkill

---
+++ New Unity Project (9)/Assets/C# Functions and Conditions/checkpoint.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class checkpoint : MonoBehaviour
+{
+    public float a;
+    public float b;
+    void Start()
+    {
+        
+
+    print(a + b);
+        print(a * b);
+        if (b != 0) {
+            print(a / b);
+        }
+    }
+    
+
+
+}
+
